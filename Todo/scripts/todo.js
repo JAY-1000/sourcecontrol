8477f509898d3f604b01c854437bcef58e1f7e12
JAY-1000 <zymerajay@gmail.com>
2022-05-27 17:04:54 +0000

    added object structure

--- Todo/scripts/todo.js
+++ Todo/scripts/todo.js
@@ -2,10 +2,17 @@ const btnCreate = document.querySelector('.btn-create');
 const todoListView = document.querySelector('.todo-lists');
 const listItems = document.querySelectorAll('.todo-item')
 
+const allTasks = [];
+// {
+//     name: "Ephraim"
+//     timestamp:""
+//     isCompleted:""
+// } 
+
 
 // CREATE TODO
 btnCreate.addEventListener('click', function () {
-    prompt("Add New Task");
+   const task = prompt("Add New Task");
 }) 
 // btnCreate.addEventListener('click',() => {})
 
