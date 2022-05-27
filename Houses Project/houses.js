d927ea79c91c20c920ab4280f3048a6308fd51b4
Caleb Crosby Adainoo <code.crosby@gmail.com>
2022-05-27 15:05:27 +0000

    feat: Houses - added descriptive comments

--- Houses Project/houses.js
+++ Houses Project/houses.js
@@ -1,10 +1,14 @@
+// getting the element by class name using the query selector
+// it stores the elements in an array
 const loves = document.querySelectorAll('.love');
 
+// loop thru the array and apply click events to all the items in the array NodeList
 loves.forEach((love) => {
+	// how to add click event to an element (eg. div, input, etc) in js
 	love.addEventListener('click', function () {
 		love.classList.toggle('loveactive');
-
+		console.log('LOVE EMENT: ', love);
 		// routing user to another link (either locale or another website)
-		window.location.href = 'https://google.com';
+		// window.location.href = 'https://google.com';
 	});
 });
