954240f4db5d9ad385587b30e1aa950a9138959d
JAY-1000 <zymerajay@gmail.com>
2022-05-27 16:58:14 +0000

    add todo list

---
+++ Todo/scripts/todo.js
@@ -0,0 +1,16 @@
+const btnCreate = document.querySelector('.btn-create');
+const todoListView = document.querySelector('.todo-lists');
+const listItems = document.querySelectorAll('.todo-item')
+
+
+// CREATE TODO
+btnCreate.addEventListener('click', function () {
+    prompt("Add New Task");
+}) 
+// btnCreate.addEventListener('click',() => {})
+
+// COMPLETE TODO
+
+// DELETE TODO
+
+// EDIT TODO
