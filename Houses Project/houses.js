ae045f1331f8048e0e02a6635bdca624a5b4c8d4
Caleb Crosby Adainoo <code.crosby@gmail.com>
2022-05-27 14:58:40 +0000

    feat: Houses - added rerouting expression to love

--- Houses Project/houses.js
+++ Houses Project/houses.js
@@ -3,5 +3,8 @@ const loves = document.querySelectorAll('.love');
 loves.forEach((love) => {
 	love.addEventListener('click', function () {
 		love.classList.toggle('loveactive');
+
+		// routing user to another link (either locale or another website)
+		window.location.href = 'https://google.com';
 	});
 });
